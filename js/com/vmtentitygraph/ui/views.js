adbf70ca7f8db8900fbcba8f1604246302fd4d6a
Tim Consolazio <tcoz@tcoz.com>
2013-01-04 13:03:20 -0500

    Checkboxes, cleanup, tweaking of overall sizes of elements.

--- js/com/vmtentitygraph/ui/views.js
+++ js/com/vmtentitygraph/ui/views.js
@@ -8,9 +8,11 @@ vmtEntityGraphView.init = function ( ) {
 
         jQuery.get ( 'js/com/vmtentitygraph/renderers/vmtEntityGraphRenderer.html',
             function ( data ) {
+
                 element = jQuery ( data ).appendTo ( '#maincontentcontainer' );
 
-                vmtEntityGraphView.drawChart ( );
+                vmtEntityGraphView.drawBackground ( );
+                vmtEntityGraphView.drawAllGraphs ( );
 
                 jQuery ( '#vmname').text ( StartupSingleton ( ).vmname );
                 // jQuery ( '#rawdata').text ( vmtEntityGraphView.getData ( ) );
@@ -21,84 +23,115 @@ vmtEntityGraphView.init = function ( ) {
     } ( ) );
 };
 
-vmtEntityGraphView.drawChart = function ( ) {
+vmtEntityGraphView.originPoint = { };
+vmtEntityGraphView.graphYMax = 100;
+vmtEntityGraphView.drawBackground = function ( ) {
 
-    var vcpuUtilizationValsArray = tcoz.parseXMLAttribute ( 'ServiceEntityHistory', 'VCPU_utilization', vmtEntityGraphView.getData ( ) ),
-        vcpuUtilizationTimeArray = tcoz.parseXMLAttribute ( 'ServiceEntityHistory', 'time', vmtEntityGraphView.getData ( ) ),
-        vmemUtilizatonArray = tcoz.parseXMLAttribute ( 'ServiceEntityHistory', 'VMem_utilization', vmtEntityGraphView.getData ( ) ),
-        storageUtilizatonArray = tcoz.parseXMLAttribute ( 'ServiceEntityHistory', 'StorageAccess_utilization', vmtEntityGraphView.getData ( ) ),
-        layer0Canvas = jQuery ( '#layer0').get ( 0 ),
-        layer0CanvasContext =  layer0Canvas.getContext ( '2d'),
-        layer1CanvasContext =  jQuery ( '#layer1').get ( 0 ).getContext ( '2d'),
-        layer2CanvasContext =  jQuery ( '#layer2').get ( 0 ).getContext ( '2d'),
-        originPoint = { 'x' : 50, 'y' : layer0Canvas.height - 25 },
+    var layer_background = jQuery ( '#layer_background').get ( 0),
+        layer_background_context = layer_background.getContext ( '2d'),
+        timeArray = tcoz.parseXMLAttribute ( 'ServiceEntityHistory', 'time', vmtEntityGraphView.getData ( ) ),
+        originPoint = vmtEntityGraphView.originPoint = { 'x' : 50, 'y' : layer_background.height - 10 },
+        max = vmtEntityGraphView.graphYMax,
+        yAxisLength = originPoint.y - 70;
         i = 0;
 
-
-    // draw the border
-    layer0CanvasContext.strokeStyle = '#000000';
-    layer0CanvasContext.strokeRect ( 0, 0, layer0Canvas.width, layer0Canvas.height );
-    layer0CanvasContext.stroke ( );
+    layer_background_context.strokeStyle = '#000000';
+    layer_background_context.strokeRect ( 0, 0, layer_background.width, layer_background.height );
+    layer_background_context.stroke ( );
 
     // draw the axis lines
-    layer0CanvasContext.beginPath ( );
-    layer0CanvasContext.moveTo ( originPoint.x - 10, originPoint.y - 20 );
-    layer0CanvasContext.lineTo ( layer0Canvas.width - 30, originPoint.y - 20 );
-    layer0CanvasContext.stroke ( );
+    layer_background_context.beginPath ( );
+    layer_background_context.moveTo ( originPoint.x - 10, originPoint.y - 20 );
+    layer_background_context.lineTo ( layer_background.width - 30, originPoint.y - 20 );
+    layer_background_context.stroke ( );
 
-    layer0CanvasContext.beginPath ( );
-    layer0CanvasContext.moveTo ( originPoint.x - 10, originPoint.y - 20 );
-    layer0CanvasContext.lineTo ( originPoint.x - 10, 40 );
-    layer0CanvasContext.stroke ( );
+    layer_background_context.beginPath ( );
+    layer_background_context.moveTo ( originPoint.x - 10, originPoint.y - 20 );
+    layer_background_context.lineTo ( originPoint.x - 10, 40 );
+    layer_background_context.stroke ( );
 
-    // set the font style
-    layer0CanvasContext.font = 'bold 10px sans-serif';
-
-    // Draw X Axis labels (compute max val and normalize to integer above it )
-    var getMaxArray = vcpuUtilizationValsArray;
-    getMaxArray.sort( function ( a, b ) { return a-b } );
-
-    var max = Math.ceil ( getMaxArray [ getMaxArray.length - 1 ] + 1 );
-    layer0CanvasContext.fillStyle = "#000000";
-    layer0CanvasContext.fillText ( '0', originPoint.x - 25, originPoint.y - 20 );
-    layer0CanvasContext.fillText ( max.toString ( ), originPoint.x - 25, 50 );
-
-    // Get times, draw Y axis labels, get values, draw indicators
-    var lastXCoord = -1;
-    var lastYCoord = -1;
-    for ( i = 0; i < vcpuUtilizationValsArray.length; i += 1 ) {
-
-        var date = new Date ( );
-        date.setTime ( vcpuUtilizationTimeArray [ i ] );
+    // draw X label times
+    var date = new Date ( );
+    for ( i = 0; i < timeArray.length; i += 1 )
+    {
+        date.setTime ( timeArray [ i ] );
 
         var xCoord = originPoint.x + ( i * 40 );
         var timeLabelVal = date.getHours ().toString ( ) + ":" + date.getMinutes ().toString ( );
 
-        layer0CanvasContext.fillStyle = "#000000";
-        layer0CanvasContext.fillText ( timeLabelVal, xCoord, originPoint.y );
+        layer_background_context.fillStyle = "#000000";
+        layer_background_context.fillText ( timeLabelVal, xCoord, originPoint.y );
+    }
 
-        var yAxisLength = originPoint.y - 50;
-        var pctOfMax = vcpuUtilizationValsArray [ i ] / max;
+    // draw the Y labels from 0 to graphYMax
+    for ( i = 0; i <= 10; i += 1 ) {
+        layer_background_context.font = 'bold 10px sans-serif';
+        layer_background_context.fillStyle = "#000000";
+        var yTarget = ( originPoint.y - 20 ) - ( ( yAxisLength * .1 ) * i );
+        layer_background_context.fillText ( ( i * 10 ).toString ( ), originPoint.x - 35, yTarget );
+    }
+};
+
+vmtEntityGraphView.drawAllGraphs = function ( ) {
+
+    // Set up and draw VCPU
+    var valsArray = tcoz.parseXMLAttribute ( 'ServiceEntityHistory', 'VCPU_utilization', vmtEntityGraphView.getData ( ) ),
+        bottom_context = jQuery ( '#layer0_vcpu').get ( 0 ).getContext ( '2d'),
+        top_context = jQuery ( '#layer1_vcpu').get ( 0 ).getContext ( '2d');
+
+    vmtEntityGraphView.drawGraph ( valsArray, bottom_context, top_context, '#FF0000', 6 );
+
+    // Set up and draw VMEM
+    valsArray = tcoz.parseXMLAttribute ( 'ServiceEntityHistory', 'VMEM_utilization', vmtEntityGraphView.getData ( ) );
+    bottom_context = jQuery ( '#layer0_vmem').get ( 0 ).getContext ( '2d');
+    top_context = jQuery ( '#layer1_vmem').get ( 0 ).getContext ( '2d');
+
+    vmtEntityGraphView.drawGraph ( valsArray, bottom_context, top_context, '#0000FF', 4 );
+
+    // Set up and draw Storage
+    valsArray = tcoz.parseXMLAttribute ( 'ServiceEntityHistory', 'Storage_utilization', vmtEntityGraphView.getData ( ) );
+    bottom_context = jQuery ( '#layer0_storage').get ( 0 ).getContext ( '2d');
+    top_context = jQuery ( '#layer1_storage').get ( 0 ).getContext ( '2d');
+
+    vmtEntityGraphView.drawGraph ( valsArray, bottom_context, top_context, '#00FF00', 2 );
+}
+
+vmtEntityGraphView.drawGraph = function ( valsArray, bottomLayer, topLayer, lineColor, lineWeight ) {
+
+    var layer_background = jQuery ( '#layer_background' ).get ( 0 ),
+        originPoint = vmtEntityGraphView.originPoint,
+        max = vmtEntityGraphView.graphYMax,
+        nudge = 10,
+        i = 0;
+
+    // Get times, draw Y axis labels, get values, draw indicators
+    var lastXCoord = -1;
+    var lastYCoord = -1;
+    for ( i = 0; i < valsArray.length; i += 1 ) {
+
+        var yAxisLength = originPoint.y - 70;
+        var pctOfMax = valsArray [ i ] / max;
+        var xCoord = originPoint.x + ( i * 40 );
         var yCoord = ( originPoint.y - 20 ) - ( yAxisLength * pctOfMax );
 
         // draw the graph lines
-        layer0CanvasContext.strokeStyle = '#FF0000';
-        layer0CanvasContext.lineWidth = 3;
+        bottomLayer.strokeStyle = lineColor;
+        bottomLayer.lineWidth = lineWeight;
         if ( lastXCoord !== -1 ) {
-            layer0CanvasContext.beginPath ( );
-            layer0CanvasContext.moveTo ( lastXCoord, lastYCoord );
-            layer0CanvasContext.lineTo ( xCoord, yCoord );
-            layer0CanvasContext.stroke ( );
+            bottomLayer.beginPath ( );
+            bottomLayer.moveTo ( lastXCoord + nudge, lastYCoord );
+            bottomLayer.lineTo ( xCoord + nudge, yCoord );
+            bottomLayer.stroke ( );
         }
 
         // draw circle indicators
-        layer1CanvasContext.fillStyle = "#00FF00";
-        layer1CanvasContext.beginPath ( );
-        layer1CanvasContext.arc ( xCoord, yCoord, 5, 0, 2 * Math.PI, false );
-        layer1CanvasContext.fill ( );
-        layer1CanvasContext.lineWidth = 1;
-        layer1CanvasContext.strokeStyle = '#000000';
-        layer1CanvasContext.stroke ( );
+        topLayer.fillStyle = "#00FF00";
+        topLayer.beginPath ( );
+        topLayer.arc ( xCoord + 10, yCoord, 5, 0, 2 * Math.PI, false );
+        topLayer.fill ( );
+        topLayer.lineWidth = 1;
+        topLayer.strokeStyle = '#000000';
+        topLayer.stroke ( );
 
         // preserve the last coords so we know the last location to a draw a line from (to this location).
         lastXCoord = xCoord;
